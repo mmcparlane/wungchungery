afeb09ae4afd82d540b9ac6aa62452d8a0bfe692
Wang Chow <wangchow1973@outlook.com>
2017-08-31 19:13:37 -0500

    Added release mode

--- src/engine.c
+++ src/engine.c
@@ -4,17 +4,16 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+
 int main (void) {
-  char buff[256];
+  char* buff = "print('A goat is in there')\n";
   int error;
   lua_State *L = luaL_newstate(); /* opens Lua */
   luaL_openlibs(L); /* opens the standard libraries */
-  while (fgets(buff, sizeof(buff), stdin) != NULL) {
-    error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
-    if (error) {
-      fprintf(stderr, "%s\n", lua_tostring(L, -1));
-      lua_pop(L, 1); /* pop error message from the stack */
-    }
+  error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
+  if (error) {
+    fprintf(stderr, "%s\n", lua_tostring(L, -1));
+    lua_pop(L, 1); /* pop error message from the stack */
   }
   lua_close(L);
   return 0;
