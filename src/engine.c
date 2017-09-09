107dfa2e174b443d50ffc26471db1e74ee44a04e
Wang Chow <wangchow1973@outlook.com>
2017-09-09 11:04:47 -0500

    Making C funcs Lua compatible.

--- src/engine.c
+++ src/engine.c
@@ -17,10 +17,12 @@
 #include <emscripten.h>
 #endif
 
-static lua_State* engine_init();
-static void engine_loop(lua_State* L);
+#define ENGINE_UPDATE_INTERVAL 50
+
+static int engine_init(lua_State* L);
+static int engine_loop(lua_State* L);
 static int engine_update(lua_State* L);
-static double engine_clock_now();
+static int engine_clock_now(lua_State* L);
 
 // TODO
 //   Add a Lua event-queue data structure which allows Lua
@@ -49,11 +51,66 @@ static double engine_clock_now();
 //        call the AI, Update, etc.
 
 
+#if defined(__EMSCRIPTEN__)
+static int engine_clock_now(lua_State* L) {
+	lua_Number now = EM_ASM_DOUBLE_V(
+		if (ENVIRONMENT_IS_NODE) {
+			var t = process.hrtime();
+			return (t[0]*1e9 + t[1])/1e6;
+		} else {
+			return performance.now();
+		});
+	lua_pushnumber(L, now);
+	return 1;
+}
+
+static void engine_em_update(void* data){
+	lua_State* L = (lua_State*)(data);
+	printf("engine_em_update: top: '%i'\n", lua_gettop(L));
+	//lua_pushcfunction(L, engine_update);
+	lua_call(L, 0, 0);
+}
+
+static int engine_loop(lua_State* L) {
+	printf("engine_loop: top: '%i'\n", lua_gettop(L));
+	emscripten_set_main_loop_arg(engine_em_update, L, 0, 0);
+	return 0;
+}
+
+
+#else
+static int engine_loop(lua_State* L) {
+	// TODO
+	//   Implement native game-loop.
+	return 0;
+}
+
+#endif
+
+
+
 static int engine_update(lua_State* L) {
-	double before = lua_tonumber(L, lua_upvalueindex(1));
-	double now = engine_clock_now();
+	double lag = lua_tonumber(L, lua_upvalueindex(1));
+	double before = lua_tonumber(L, lua_upvalueindex(2));
+
+	lua_pushcfunction(L, engine_clock_now);
+	lua_call(L, 0, 1);
 	
-	printf("tick delta: %f')\n", (now-before));
+	double now = luaL_checknumber(L, 1);
+	double gap = now - before;
+	lag += gap;
+
+	printf("lag: '%f', before: '%f', now: '%f', gap: '%f'\n",
+	       lag, before, now, gap);
+
+	// input();
+
+	while (lag >= ENGINE_UPDATE_INTERVAL) {
+		// update();		
+		lag -= ENGINE_UPDATE_INTERVAL;
+	}
+
+	// render(lag / ENGINE_UPDATE_INTERVAL);
 	
 	/*
 	int error;
@@ -65,53 +122,31 @@ static int engine_update(lua_State* L) {
 	}
 	*/
 
-	lua_pushnumber(L, now);
+	lua_pushnumber(L, lag);
 	lua_copy(L, -1, lua_upvalueindex(1));
+	lua_pushnumber(L, now);
+	lua_copy(L, -1, lua_upvalueindex(2));
 
 	return 0;
 }
 
-static lua_State* engine_init() {
-	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
-	
-	lua_pushnumber(L, engine_clock_now());
-	lua_pushcclosure(L, engine_update, 1);
+static int engine_init(lua_State* L) {
+	printf("d\n");
+	lua_pushnumber(L, 50.0);
+	lua_pushcfunction(L, engine_clock_now);
+	lua_call(L, 0, 1);
+	lua_pushcclosure(L, engine_update, 2);
 
-	return L;
+	return 1;
 }
-
-
-#if defined(__EMSCRIPTEN__)
-static double engine_clock_now() {
-	return EM_ASM_DOUBLE_V(
-		if (ENVIRONMENT_IS_NODE) {
-			var t = process.hrtime();
-			return (t[0]*1e9 + t[1])/1e6;
-		} else {
-			return performance.now();
-		});
-}
-
-static void engine_em_update(void* data){
-	lua_State* L = (lua_State*)(data);
-	lua_pushcfunction(L, engine_update);
-	lua_call(L, 0, 0);
-}
-
-static void engine_loop(lua_State* L) {
-	emscripten_set_main_loop_arg(engine_em_update, L, 0, 0);
-}
-
-#else
-static void engine_loop(lua_State* L) {
-	// TODO
-	//   Implement native game-loop.
-}
-
-#endif
 
 
 int main (int argc, char* argv[]) {
-	engine_loop(engine_init());
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	lua_pushcfunction(L, engine_loop);
+	lua_pushcfunction(L, engine_init);
+	lua_call(L, 0, 1);
+	lua_call(L, 1, 0);
+	printf("c\n");
 }
