48ebf2ce2af55d2f1de2f3e49a1498cb972e4f7b
Wang Chow <wangchow1973@outlook.com>
2017-09-08 19:39:57 -0500

    Added emscripten tick algorithm.

--- src/engine.c
+++ src/engine.c
@@ -13,6 +13,14 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
+#if defined(__EMSCRIPTEN__)
+#include <emscripten.h>
+#endif
+
+static lua_State* engine_init();
+static void engine_loop(lua_State* L);
+static int engine_tick(lua_State* L);
+static double engine_clock_now();
 
 // TODO
 //   Add a Lua event-queue data structure which allows Lua
@@ -40,19 +48,14 @@
 //      - The game loop then proceeds to
 //        call the AI, Update, etc.
 
-typedef struct wch_GameState wch_GameState;
-struct wch_GameState {
-	lua_State* luaState;
-};
 
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-
-static void engine_em_update(void* data){
-	wch_GameState* game = (wch_GameState*)(data);
-	lua_State* L = game->luaState;
+static int engine_update(lua_State* L) {
+	double before = lua_tonumber(L, lua_upvalueindex(1));
+	double now = engine_clock_now();
 	
-	char* buff = "print('A goat is in there')\n";
+	printf("tick delta: %f')\n", (now-before));
+	
+	/*
 	int error;
 
 	error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
@@ -60,14 +63,48 @@ static void engine_em_update(void* data){
 		fprintf(stderr, "%s\n", lua_tostring(L, -1));
 		lua_pop(L, 1);
 	}
+	*/
+
+	lua_pushnumber(L, now);
+	lua_copy(L, -1, lua_upvalueindex(1));
+
+	return 0;
 }
 
-static void engine_loop(wch_GameState* game) {
-	emscripten_set_main_loop_arg(engine_em_update, game, 10, 0);
+static lua_State* engine_init() {
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+	
+	lua_pushnumber(L, engine_clock_now());
+	lua_pushcclosure(L, engine_update, 1);
+
+	return L;
+}
+
+
+#if defined(__EMSCRIPTEN__)
+static double engine_clock_now() {
+	return EM_ASM_DOUBLE_V(
+		if (ENVIRONMENT_IS_NODE) {
+			var t = process.hrtime();
+			return (t[0]*1e9 + t[1])/1e6;
+		} else {
+			return performance.now();
+		});
+}
+
+static void engine_em_update(void* data){
+	lua_State* L = (lua_State*)(data);
+	lua_pushcfunction(L, engine_update);
+	lua_call(L, 0, 0);
+}
+
+static void engine_loop(lua_State* L) {
+	emscripten_set_main_loop_arg(engine_em_update, L, 0, 0);
 }
 
 #else
-static void engine_loop(wch_GameState* game) {
+static void engine_loop(lua_State* L) {
 	// TODO
 	//   Implement native game-loop.
 }
@@ -76,16 +113,5 @@ static void engine_loop(wch_GameState* game) {
 
 
 int main (int argc, char* argv[]) {
-	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
-	engine_loop(L);
-	// TODO
-	//  Need to have a way to wait for the loop to signal cleanup
-	//  routines to run before returning from main(). This probably
-	//  is best done as an event-queue-registered handler as noted
-	//  above.
-	/*
-	lua_close(L);
-	*/
-	return 0;
+	engine_loop(engine_init());
 }
