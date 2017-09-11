043b6ae742dc178b901c371610af4ee16877f011
Wang Chow <wangchow1973@outlook.com>
2017-09-11 11:21:00 -0500

    Checking equeue in before removing bloat... Learning the Lua API!

--- src/equeue.c
+++ src/equeue.c
@@ -52,31 +52,45 @@ static int equeue_is_empty(lua_State* L) {
 }
 
 static int equeue_add_handler(lua_State* L) {
-	// Each event ID can have multiple handlers
-	// and so each event ID needs to map to a
-	// table.
-
-	// If no handlers exist for a particular ID,
-	// create a table and add the event to the
-	// table.
-	if (! lua_istable(L, -3)) {
-		luaL_error(L,
-			   "bad argument #1 to 'equeue_add_handler'"
-			   "(table expected, got %s)",
-			   lua_typename(L, lua_type(L, -3)));
+	if (! lua_istable(L, 1)) {
+		luaL_error(
+			L,
+			"bad argument #1 to 'equeue_add_handler'"
+			"(table expected, got %s)",
+			lua_typename(L, lua_type(L, 1)));
 	}
-	lua_Integer id = luaL_checkinteger(L, -2);
-	if (! lua_isfunction(L, -1)) {
-		luaL_error(L,
-			   "bad argument #3 to 'equeue_add_handler'"
-			   "(function expected, got %s)",
-			   lua_typename(L, lua_type(L, -1)));
-	} else {
-		// Fetch sub-table self.handlers.id
-		// self is already on the stack so
-		// fetch [-3].handlers
-		// fetch top.id
+	lua_Integer id = luaL_checkinteger(L, 2);
+	if (! lua_isfunction(L, 3)) {
+		return luaL_error(
+			L,
+			"bad argument #3 to 'equeue_add_handler'"
+			"(function expected, got %s)",
+			lua_typename(L, lua_type(L, 3)));
+	}
+
+	luaL_getsubtable(L, "handlers");
+	{
+		lua_pushinteger(L, id);
 		
+		switch(lua_gettable(L, -2)) {	
+		case LUA_TTABLE:
+			break;
+			
+		case LUA_TNIL:
+			lua_pushinteger(L, id);
+			lua_newtable(L);
+			lua_settable(L, -3);
+			break;
+			
+		default:
+			return luaL_error(
+				L,
+				"Event ID '%i' maps to invalid type '%s'. "
+				"Table expected (so multiple event handlers "
+				"can handle a single event).",
+				id, lua_typename(L, lua_type(L, -1)));
+		}
+
 		
 	}
 }
