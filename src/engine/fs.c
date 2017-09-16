16c996cdeb2a82412f5c3362cbc1b4b67766505c
Wang Chow <wangchow1973@outlook.com>
2017-09-16 17:10:10 -0500

    Impld. several fs functions

--- src/engine/fs.c
+++ src/engine/fs.c
@@ -17,10 +17,10 @@ static int fs_mount(lua_State* L) {
 	const char* virtual = luaL_checkstring(L, 2);
 
 	lua_pushfstring(L,
-			"    if (ENVIRONMENT_IS_NODE) {"
-			"        FS.mkdir('%s');"
-			"        FS.mount(NODEFS, {root: '%s'}, '%s');"
-			"    }",
+			"if (ENVIRONMENT_IS_NODE) {"
+			"    FS.mkdir('%s');"
+			"    FS.mount(NODEFS, {root: '%s'}, '%s');"
+			"}",
 			virtual, real, virtual);
 	
 	emscripten_run_script(lua_tostring(L, -1));
@@ -33,28 +33,45 @@ static int fs_unmount(lua_State* L) {
 
 	lua_pushfstring(L,"FS.unmount('%s');", virtual);
 	emscripten_run_script(lua_tostring(L, -1));
-	
+
 	return 0;
 }
 
 static int fs_mkdir(lua_State* L) {
-	// Not implemented
+	const char* path = luaL_checkstring(L, 1);
+
+	lua_pushfstring(L,"FS.mkdir('%s');", path);
+	emscripten_run_script(lua_tostring(L, -1));
+	
 	return 0;
 }
 
 static int fs_rmdir(lua_State* L) {
-	// Not implemented
+	const char*  path = luaL_checkstring(L, 1);
+
+	lua_pushfstring(L, "FS.rmdir('%s');", path);
+	emscripten_run_script(lua_tostring(L, -1));
+	
 	return 0;
 }
 
 static int fs_ls(lua_State* L) {
-	// Not implemented
-	return 0;
+	const char* path = luaL_checkstring(L, 1);
+
+	lua_pushfstring(L,
+			"if (FS.isDir('%s')) }"
+			"    FS.readFile('%s', {encoding: 'utf8', flags: 'r'});"
+			"}",
+			path, path);
+        lua_pushstring(L, emscripten_run_script_string(lua_tostring(L, -1)));
+	
+	return 1;
 }
 
 static int fs_pwd(lua_State* L) {
-	// Not implemented
-	return 0;
+	lua_pushstring(L, emscripten_run_script_string("FS.cwd();"));
+	
+	return 1;
 }
 
 
