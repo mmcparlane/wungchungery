b67f159b7499c5f17b5d01a91d1b5124f2701086
Wang Chow <wangchow1973@outlook.com>
2017-09-16 15:57:05 -0500

    Implemented mount/unmount.

--- src/engine/fs.c
+++ src/engine/fs.c
@@ -10,8 +10,61 @@
 #include <emscripten.h>
 #endif
 
+
 #if defined(__EMSCRIPTEN__)
-static int fs_new(lua_State* L) {
+static int fs_mount(lua_State* L) {
+        const char* real = luaL_checkstring(L, 1);
+	const char* virtual = luaL_checkstring(L, 2);
+
+	lua_pushfstring(L,
+			"    if (ENVIRONMENT_IS_NODE) {"
+			"        FS.mkdir('%s');"
+			"        FS.mount(NODEFS, {root: '%s'}, '%s');"
+			"    }",
+			virtual, real, virtual);
+	
+	emscripten_run_script(lua_tostring(L, -1));
+	
+	return 0;
+}
+
+static int fs_unmount(lua_State* L) {
+	const char* virtual = luaL_checkstring(L, 1);
+
+	lua_pushfstring(L,"FS.unmount('%s');", virtual);
+	emscripten_run_script(lua_tostring(L, -1));
+	
+	return 0;
+}
+
+static int fs_mkdir(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
+static int fs_rmdir(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
+static int fs_ls(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
+static int fs_pwd(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
+
+#else
+static int fs_mount(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
+static int fs_unmount(lua_State* L) {
 	// Not implemented
 	return 0;
 }
@@ -21,7 +74,17 @@ static int fs_mkdir(lua_State* L) {
 	return 0;
 }
 
+static int fs_rmdir(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
 static int fs_ls(lua_State* L) {
+	// Not implemented
+	return 0;
+}
+
+static int fs_pwd(lua_State* L) {
 	// Not implemented
 	return 0;
 }
@@ -29,9 +92,12 @@ static int fs_ls(lua_State* L) {
 
 
 static const luaL_Reg fs_lib[] = {
-	{"new", fs_new},
+	{"mount", fs_mount},
+	{"unmount", fs_unmount},
 	{"mkdir", fs_mkdir},
+	{"rmdir", fs_rmdir},
 	{"ls", fs_ls},
+	{"pwd", fs_pwd},
 	{NULL, NULL},
 };
 
