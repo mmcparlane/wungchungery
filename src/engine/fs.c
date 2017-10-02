c7a29ad1d6361d9980d0754811a64a28654ceaa0
Wang Chow <wangchow1973@outlook.com>
2017-10-01 19:22:18 -0500

    Modified ls to return table

--- src/engine/fs.c
+++ src/engine/fs.c
@@ -31,6 +31,7 @@ static int fs_rmdir(lua_State* L) {
 }
 
 static int fs_ls(lua_State* L) {
+	int iresult = 0, iiter = 0;
 	const char* path = luaL_gsub(L, luaL_checkstring(L, 1), "\\", "/");
 	const char* type = "all";
 	if (lua_isstring(L, 2)) type = lua_tostring(L, 2);
@@ -68,7 +69,27 @@ static int fs_ls(lua_State* L) {
 		"    return r.join('\\n');"
 		" })('%s', '%s')", path, type);
 
+	lua_newtable(L);
+	iresult = lua_gettop(L);
+	
+	lua_getglobal(L, "string");
+	lua_getfield(L, -1, "gmatch");
         lua_pushstring(L, emscripten_run_script_string(lua_tostring(L, -1)));
+	lua_pushstring(L, ".+\\n");
+	lua_call(L, 2, 1);
+	iiter = lua_gettop(L);
+
+	// for f in iter do r[#r + 1] = f end
+        lua_pushvalue(L, iiter);
+	lua_call(L, 0, 1);
+	while(lua_isstring(L, -1)) {
+		lua_rawseti(L, iresult, lua_rawlen(L, iresult)+1);
+		
+		lua_pushvalue(L, iiter);
+		lua_call(L, 0, 1);
+	}
+
+	lua_pushvalue(L, iresult);
 	
 	return 1;
 }
@@ -102,34 +123,63 @@ static int fs_pwd(lua_State* L) {
 }
 #endif
 
+
+#define FS_LS(L, PATH, TYPE) do { \
+		lua_pushcfunction(L, fs_ls); \
+		lua_pushstring(L, (PATH)); \
+		lua_pushstring(L, (TYPE)); \
+		lua_call(L, 2, 1); \
+	} while(0)
+
 static int fs_find(lua_State* L) {
-	int istring = 0, itable = 0, iresult = 0, iiter = 0, ichildren = 0;
+	int istring = 0, itable = 0, iresult = 0, idirs = 0, ifiles = 0;
 	const char* path = luaL_checkstring(L, 1);
 	const char* pattern = luaL_checkstring(L, 2);
-
-	lua_getglobal(L, "string"); istring = lua_gettop(L);
-	lua_getglobal(L, "table"); itable = lua_gettop(L);
-	lua_newtable(L); iresult = lua_gettop(L);
-
-	// children = fs.ls(path)
-	lua_pushcfunction(L, fs_ls);
-	lua_pushstring(L, path);
-	lua_call(L, 1, 1);
-	ichildren = lua_gettop(L);
-
-	// iter = string.gmatch(children, pattern)
-	lua_getfield(L, istring, "gmatch");
-	lua_pushvalue(L, ichildren);
-	lua_pushstring(L, pattern);
-	lua_call(L, 2, 1);
-	iiter = lua_gettop(L);
-
-        lua_pushvalue(L, iiter); lua_call(L, 0, 1);
-	while(lua_isstring(L, -1)) {
-		lua_rawseti(L, iresult, lua_rawlen(L, iresult)+1);
-		lua_pushvalue(L, iiter); lua_call(L, 0, 1);
+	if (! lua_istable(L, 3)) {
+		lua_newtable(L);
+		iresult = lua_gettop(L);
 	}
+
+	lua_getglobal(L, "string");
+	istring = lua_gettop(L);
 	
+	lua_getglobal(L, "table");
+	itable = lua_gettop(L);
+
+	FS_LS(L, path, "file");
+	ifiles = lua_gettop(L);
+
+	lua_pushnil(L);
+	while (lua_next(L, ifiles)) {
+		lua_getfield(L, istring, "find");
+		lua_pushvalue(L, -2);
+		lua_pushstring(L, pattern);
+		lua_call(L, 2, 1);
+		if (! lua_isnil(L, -1)) {
+			lua_pop(L, 1);
+		        lua_rawseti(L, iresult, lua_rawlen(L, iresult)+1);
+		} else {
+			lua_pop(L, 2);
+		}
+	}
+
+	FS_LS(L, path, "dir");
+	idirs = lua_gettop(L);
+
+	if (lua_rawlen(L, -1) > 0) {
+		lua_pushnil(L);
+		while (lua_next(L, idirs)) {
+			lua_pushcfunction(L, fs_find);
+			lua_pushvalue(L, -2);
+			lua_pushstring(L, pattern);
+			lua_pushvalue(L, iresult);
+			lua_call(L, 3, 0);
+			
+			lua_pop(L, 1);
+		}
+	}
+
+	lua_pushvalue(L, iresult);
 	return 1;
 }
 
